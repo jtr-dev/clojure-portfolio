30538661b0b0da6ef8b90cc3f19775ad6ee151e1
Tyler Roberts <dnbxna@gmail.com>
2017-02-28 15:02:52 -0500

    test(input) cond

--- src/portfolio/core.cljs
+++ src/portfolio/core.cljs
@@ -4,14 +4,38 @@
 ;; -------------------------
 ;; Views
 
+; (defonce site-name
+;   (reagent/atom
+;    {:text ""}))
+
+(defonce app-state
+  (reagent/atom
+   {:text "" :color "rgba(50, 50, 255, 0.2)"}))
+
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  [:div [:h2 "Welcome tto Reagent"]])
+
+;; Page
+(defn paragraph [ratom]
+  (println (:color @ratom))
+  (when (> (count (:text @ratom)) 0)
+   [:div {:style {:background-color (:color @ratom) :width 160}}
+    [:p "Hi,  "(:text @ratom)]]))
+
+
+(defn page [ratom]
+  (let [guestname (fn [e]
+                   (swap! ratom assoc :text e.target.value))]
+   [:p "Hello, what is your name?"
+    [:p [:input {:type "text" :on-change guestname}]]
+    [paragraph ratom]]))
+
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (reagent/render [home-page] (.getElementById js/document "app")))
+  (reagent/render [page app-state] (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
