d9509b0f0316fdb20030c595273c0ccb96c02358
Tyler Roberts <Tyler Roberts>
2017-03-01 00:41:07 -0500

    feat(add) routes and portfolio page

--- src/portfolio/core.cljs
+++ src/portfolio/core.cljs
@@ -1,56 +1,100 @@
 (ns portfolio.core
-    (:require [reagent.core :as reagent]))
+    (:require [reagent.core :as reagent]
+              [secretary.core :as secretary]
+              [goog.events :as events]
+              [goog.history.EventType :as EventType]
+              [reagent.core :as reagent])
+    (:require-macros [secretary.core :refer [defroute]])
+    (:import goog.History))
+
+
+(defn hook-browser-navigation! []
+      (doto (History.)
+        (events/listen
+         EventType/NAVIGATE
+         (fn [event]
+           (secretary/dispatch! (.-token event))))
+        (.setEnabled true)))
 
 ;; -------------------------
 ;; Views
-
 (defonce app-state
-  (reagent/atom
-   {:text "" :color "rgba(50, 50, 255, 0.2)"}))
+  (reagent/atom {}))
+
+(defn app-routes []
+  (secretary/set-config! :prefix "#"))
+
+(defroute "/" []
+      (swap! app-state assoc :page :home))
+
+(defroute "/about" []
+      (swap! app-state assoc :page :about))
+
+(defroute "/portfolio" []
+      (swap! app-state assoc :page :portfolio))
+
+(hook-browser-navigation!)
+
+(defmulti current-page #(@app-state :page))
+(defmethod current-page :home []
+  [:div "Home page"])
+
+(defmethod current-page :about []
+  [:div "About page"
+   [:button "CLICK ME"]])
+
+(defmethod current-page :portfolio []
+ (repeat 6
+  [:div.a
+   [:div.b]
+   [:div.c
+    [:div.c--inner
+     [:h3 "Portfolio Thing"]
+     [:p.subtitle "Catchy subtitle"]
+     [:a.cta {:href "#"} "GO"]]]]))
+
+(defn now [] (new java.util.Date))
+
+(defn footer []
+ [:div.footer ])
+
+
+(defmethod current-page :contact [])
 
 
 
 (defn navbar []
- [:div.container-fluid.darkbg
-  [:div
+ [:div#menu
+  [:nav
    [:ul
-    [:div.mainnav
-     [:li [:a {:href "/"} "Home"]]]]]])
+    [:li
+     [:a {:href "#/"}
+      [:span "Home"]]]
+    [:li
+     [:a {:href "#/about"}
+      [:span "About"]]]
+    [:li
+     [:a {:href "#/portfolio"}
+      [:span "Portfolio"]]]
+    [:li
+     [:a {:href "#"}
+      [:span "Contact"]]]]]])
 
-(defn paragraph [ratom]
-  (when (> (count (:text @ratom)) 0)
-   [:div {:style {:background-color (:color @ratom) :width 160}}
-    [:p "Hi, "(:text @ratom)]]))
-
-(defn page [ratom]
-  (let [guestname (fn [e]
-                   (swap! ratom assoc :text e.target.value))]
-   [:p "Hello, what is your name?"
-    [:p [:input {:type "text" :on-change guestname}]]
-    [paragraph ratom]]))
-
-
-(defn project-card []
-  [:div.card.card-1
-   [:div.card-title "My Blog"
-    [:hr]
-    [:div [:img.card-image {:src "https://dummyimage.com/250x250/000/fff.png"}]]]])
-
-(defn footer []
-  [:div.footer "2017"])
 
 ;; View
 
 (defn portfolio-app [props]
-  [:div.navbar (navbar)
-   [:section.portfolio-app
-     [:div (project-card)]]
-   [:footer (footer)]])
+  [:div (navbar)
+   [:hr]
+   (current-page)
+   [:hr]
+   [:footer.footer (footer)]])
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
+  (app-routes)
   (reagent/render [portfolio-app] (.getElementById js/document "app")))
 
 (defn init! []
